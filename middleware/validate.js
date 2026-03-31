040dbeeb47c47b4c82b1e4757a7dd40edaef4606
Pooja Bhayde <78949822+PoojaBhayde@users.noreply.github.com>
2026-03-31 16:09:11 +0530

    Add validation middleware for request bodies

---
+++ middleware/validate.js
@@ -0,0 +1,17 @@
+module.exports = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false // show all errors
+    });
+
+    if (error) {
+      const messages = error.details.map(err => err.message);
+
+      return res.status(400).json({
+        errors: messages
+      });
+    }
+
+    next(); // go to controller if valid
+  };
+};
