66c69aa18f13cc2be46b0a00ccb861b0034d4f3b
Pooja Bhayde <78949822+PoojaBhayde@users.noreply.github.com>
2026-03-31 16:06:44 +0530

    Enhance leaveSchema with validation messages
    Updated leaveSchema to include custom messages for name and date validation.

--- validators/leaveValidator.js
+++ validators/leaveValidator.js
@@ -2,13 +2,19 @@
 const Joi = require('joi');
 
 exports.leaveSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(3).max(50).required().messages({
+    'string.empty': 'Name is required',
+    'string.min': 'Name must be at least 3 characters',
+  }),
 
   fromDate: Joi.date().required(),
 
   toDate: Joi.date()
     .greater(Joi.ref('fromDate'))
-    .required(),
+    .required()
+    .messages({
+      'date.greater': 'To date must be greater than from date'
+    }),
 
-  halfDayCheck: Joi.boolean().required()
+  halfDayCheck: Joi.boolean().default(false)
 });
