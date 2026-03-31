9e235fcda65050035f9aa68bbe74d2de1ee0392c
Pooja Bhayde <78949822+PoojaBhayde@users.noreply.github.com>
2026-03-31 15:51:24 +0530

    Add leave validation schema using Joi

--- validators/leaveValidator.js
+++ validators/leaveValidator.js
@@ -1,1 +1,14 @@
 
+const Joi = require('joi');
+
+exports.leaveSchema = Joi.object({
+  name: Joi.string().required(),
+
+  fromDate: Joi.date().required(),
+
+  toDate: Joi.date()
+    .greater(Joi.ref('fromDate'))
+    .required(),
+
+  halfDayCheck: Joi.boolean().required()
+});
