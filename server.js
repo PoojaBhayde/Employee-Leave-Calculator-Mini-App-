dacb66394f09e9d42b8d1369e74b107d1d63bc21
Pooja Bhayde <78949822+PoojaBhayde@users.noreply.github.com>
2026-03-31 14:46:57 +0530

    Initialize Express server with leave routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const leaveRoutes = require('./routes/leave');
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/leave', leaveRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
